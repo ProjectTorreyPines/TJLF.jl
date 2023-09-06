16c975fcbe9632c9c7d34af91ea383d739c5a185
dannysun <dannysun@princeton.edu>
2023-09-06 14:30:26 -0700

    tested some dot operation

--- src/test2.jl
+++ src/test2.jl
@@ -3,6 +3,9 @@ function a()
 
 asdf=0
 println(asdf)
+println(2π
++3)
+if(true) println("one line") end
 end
 
 module test2
@@ -10,5 +13,20 @@ export nb
 nb = 1
 
 end
+
+arr=[1,2,3,4,5,6]
+b=[1,2,3,4,5,6]
+# c=zero(6)
+arr[1]+=1
+c =arr.*b
+c[1] = 4
+println(c)
+println(arr)
+println(.√(b.^2 .+ b))
+
+d = zeros(6)
+d .= arr
+d[1]+=1
+println(arr)
 
 a()
