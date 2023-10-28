843cc269a670d72afb50bc1d14d250b1c58f057e
dannysun <dannysun@alumni.princeton.edu>
2023-10-27 22:34:44 -0700

    made regression tests sum over QL fluxes

--- test/runtests_regressions.jl
+++ test/runtests_regressions.jl
@@ -8,16 +8,34 @@ using ..TJLF
 directory = "../outputs/tglf_regression/"
 tests = readdir(directory)
 
-excludeFolders = ["tglf05"]
+# 03 is s-alpha geometry
+# 04 is alpha_quench != 0.0
+# 05 is ns = 3
+# 09 is use_bper = true
+excludeFolders = ["tglf03", "tglf04", "tglf05", "tglf09"]
 testFolders = [joinpath(directory,item) for item in readdir(directory) if isdir(joinpath(directory,item)) && item ∉ excludeFolders]
 
 for baseDirectory in testFolders
-    
-    inputTJLF = readInput(baseDirectory)
-    println(dir_name)
-    fluxes = TJLF.run(inputTJLF)
-    
-    @assert !isnan(sum(fluxes))
+    @testset "$baseDirectory" begin
+        
+        fileDirectory = joinpath(baseDirectory, "out.tglf.QL_flux_spectrum")
+        lines = readlines(fileDirectory)
+        (ntype, nspecies, nfield, nky, nmodes) = parse.(Int32, split(lines[4]))
+        fluxesFortran = Vector{Float64}()
+        for line in lines[7:length(lines)]
+            line = split(line)
+            if any(occursin.(["m","s"],string(line))) continue end
+            for x in line
+                push!(fluxesFortran,parse(Float64, string(x)))
+            end
+        end
+        
+        inputTJLF = readInput(baseDirectory)
+        println(baseDirectory)
+        fluxesJulia = TJLF.run(inputTJLF)
+        
+        @test isapprox(sum(fluxesJulia),sum(fluxesFortran), rtol=1e-6)
+    end
 
 end
 
