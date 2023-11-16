3bbdf7ba904c89a356e2f7e9b18c78f9e0b65e9a
dannysun <dannysun@alumni.princeton.edu>
2023-11-16 15:08:57 -0800

    ORSO hack

--- src/run_tjlf.jl
+++ src/run_tjlf.jl
@@ -3,6 +3,9 @@ function run(inputTJLF::InputTJLF)
     satParams = get_sat_params(inputTJLF)
     inputTJLF.KY_SPECTRUM .= get_ky_spectrum(inputTJLF, satParams.grad_r0)
     QL_weights, eigenvalue = tjlf_TM(inputTJLF, satParams, outputHermite)
+    # for i in 1:size(eigenvalue,1)
+    #     eigenvalue[i,1,1] = min(eigenvalue[i,1,1]/inputTJLF.KY_SPECTRUM[1], eigenvalue[i,2,1]/inputTJLF.KY_SPECTRUM[2]*.99)*inputTJLF.KY_SPECTRUM[1]
+    # end -ORSO hack
     QL_flux_out, flux_spectrum = sum_ky_spectrum(inputTJLF, satParams, eigenvalue[:,:,1], QL_weights)
     return QL_weights, eigenvalue, QL_flux_out, flux_spectrum
 end
