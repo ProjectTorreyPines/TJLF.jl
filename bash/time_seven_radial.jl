7f7de35bed52258230c054581d1b685de2a3d4e9
Tim Slendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2023-12-13 12:04:16 -0800

    make it a loop @dannysun91 :fire:

--- bash/time_seven_radial.jl
+++ bash/time_seven_radial.jl
@@ -6,23 +6,12 @@ using BenchmarkTools
 using LinearAlgebra
 BLAS.set_num_threads(1)
 
-inputTJLFVector = Vector{Main.TJLF.InputTJLF}(undef, 7)
+N_cases = 7
+inputTJLFVector = Vector{Main.TJLF.InputTJLF}(undef, N_cases)
 
-baseDirectory = "../outputs/TIM_case/case2/"
-inputTJLFVector[1] = readInput(baseDirectory*"input.tglf")
-baseDirectory = "../outputs/TIM_case/case3/"
-inputTJLFVector[2] = readInput(baseDirectory*"input.tglf")
-baseDirectory = "../outputs/TIM_case/case4/"
-inputTJLFVector[3] = readInput(baseDirectory*"input.tglf")
-baseDirectory = "../outputs/TIM_case/case5/"
-inputTJLFVector[4] = readInput(baseDirectory*"input.tglf")
-baseDirectory = "../outputs/TIM_case/case6/"
-inputTJLFVector[5] = readInput(baseDirectory*"input.tglf")
-baseDirectory = "../outputs/TIM_case/case7/"
-inputTJLFVector[6] = readInput(baseDirectory*"input.tglf")
-baseDirectory = "../outputs/TIM_case/case8/"
-inputTJLFVector[7] = readInput(baseDirectory*"input.tglf")
-
+for case in 1:N_cases
+    inputTJLFVector[case] = "../outputs/TIM_case/case$(case+1)/input.tglf"
+end
 TJLF.run_tjlf(inputTJLFVector)
 
 @btime TJLF.run_tjlf(inputTJLFVector) |> x -> nothing
