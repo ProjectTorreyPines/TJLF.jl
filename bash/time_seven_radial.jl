109bbe9b3df0fa627d9a17fbfecd848b3a4bbdf2
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2023-12-13 13:33:58 -0800

    one-liner per @bclyons12  @orso82  with path fix and tested
    Co-Authored-By: Brendan Lyons <bclyons12@users.noreply.github.com>
    Co-Authored-By: Orso Meneghini <1537880+orso82@users.noreply.github.com>

--- bash/time_seven_radial.jl
+++ bash/time_seven_radial.jl
@@ -1,5 +1,6 @@
 using Pkg
-Pkg.activate("..")
+path = joinpath(@__DIR__, "..")
+Pkg.activate(path)
 Pkg.build("TJLF")
 using TJLF
 using BenchmarkTools
@@ -7,11 +8,7 @@ using LinearAlgebra
 BLAS.set_num_threads(1)
 
 N_cases = 7
-inputTJLFVector = Vector{Main.TJLF.InputTJLF}(undef, N_cases)
-
-for case in 1:N_cases
-    inputTJLFVector[case] = "../outputs/TIM_case/case$(case+1)/input.tglf"
-end
+inputTJLFVector = TJLF.InputTJLF[readInput("$path/outputs/TIM_case/case$(case+1)/input.tglf") for case in 1:N_cases]
 TJLF.run_tjlf(inputTJLFVector)
 
 @btime TJLF.run_tjlf(inputTJLFVector) |> x -> nothing
