60882433ac1c97cf039e769252d4ac584ac48f90
Orso Meneghini <orso82@gmail.com>
2024-03-21 15:47:18 -0700

    avoid code duplication
    mention @tomneiser

--- src/run_tjlf.jl
+++ src/run_tjlf.jl
@@ -4,26 +4,17 @@ function run(inputTJLF::InputTJLF)
     satParams = get_sat_params(inputTJLF)
     inputTJLF.KY_SPECTRUM .= get_ky_spectrum(inputTJLF, satParams.grad_r0)
     QL_weights, eigenvalue = tjlf_TM(inputTJLF, satParams, outputHermite)
-    # for i in 1:size(eigenvalue,1)
-    #     eigenvalue[i,1,1] = min(eigenvalue[i,1,1]/inputTJLF.KY_SPECTRUM[1], eigenvalue[i,2,1]/inputTJLF.KY_SPECTRUM[2]*.99)*inputTJLF.KY_SPECTRUM[1]
-    # end -ORSO hack
-    QL_flux_out, flux_spectrum = sum_ky_spectrum(inputTJLF, satParams, eigenvalue[:,:,1], QL_weights)
-    return QL_weights, eigenvalue, QL_flux_out, flux_spectrum
+    QL_flux_out, flux_spectrum = sum_ky_spectrum(inputTJLF, satParams, eigenvalue[:, :, 1], QL_weights)
+    return (QL_weights=QL_weights, eigenvalue=eigenvalue, QL_flux_out=QL_flux_out, flux_spectrum=flux_spectrum)
 end
+
 function run(inputTGLF::InputTGLF)
     inputTJLF = InputTJLF{Float64}(inputTGLF)
-    checkInput(inputTJLF)
-    outputHermite = gauss_hermite(inputTJLF)
-    satParams = get_sat_params(inputTJLF)
-    inputTJLF.KY_SPECTRUM .= get_ky_spectrum(inputTJLF, satParams.grad_r0)
-    QL_weights, eigenvalue = tjlf_TM(inputTJLF, satParams, outputHermite)
-    QL_flux_out, flux_spectrum = sum_ky_spectrum(inputTJLF, satParams, eigenvalue[:,:,1], QL_weights)
-    return QL_weights, eigenvalue, QL_flux_out, flux_spectrum
+    return run(inputTJLF)
 end
 
-
 """
-    function run_tjlf(input_tjlfs::Vector{InputTJLF})
+    run_tjlf(inputTJLF::InputTJLF)
 
 parameters:
     input_tjlfs::InputTJLF                  - InputTJLF struct
@@ -33,15 +24,11 @@ description:
     If you want to use these widths and eigenvalues in future runs, there is a flag: FIND_WIDTH and FIND_EIGEN that you set to false
 """
 function run_tjlf(inputTJLF::InputTJLF)
-    checkInput(inputTJLF)
-    outputHermite = gauss_hermite(inputTJLF)
-    satParams = get_sat_params(inputTJLF)
-    inputTJLF.KY_SPECTRUM .= get_ky_spectrum(inputTJLF, satParams.grad_r0)
-    QL_weights, eigenvalue = tjlf_TM(inputTJLF, satParams, outputHermite)
-    return  sum_ky_spectrum(inputTJLF, satParams, eigenvalue[:,:,1], QL_weights)[1]
+    return run(inputTJLF).QL_flux_out
 end
+
 """
-    function run_tjlf(input_tjlfs::Vector{InputTJLF})
+    run_tjlf(input_tjlfs::Vector{InputTJLF})
 
 parameters:
     input_tjlfs::Vector{InputTJLF}          - vector of InputTJLF structs
@@ -55,10 +42,9 @@ description:
 """
 function run_tjlf(input_tjlfs::Vector{InputTJLF})
     checkInput(input_tjlfs)
-    outputs = Vector{Array{Float64, 3}}(undef,length(input_tjlfs))
+    outputs = Vector{Array{Float64,3}}(undef, length(input_tjlfs))
     Threads.@threads for idx in eachindex(input_tjlfs)
         outputs[idx] = TJLF.run_tjlf(input_tjlfs[idx])
     end
-
     return outputs
 end
