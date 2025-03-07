d90085e99c1507e4413d3788e0f3ef3a8ac403ad
Orso Meneghini <orso82@gmail.com>
2025-03-06 21:57:08 -0800

    minor

--- test/runtests_regressions.jl
+++ test/runtests_regressions.jl
@@ -20,10 +20,10 @@ for baseDirectory in testFolders
         fluxesJulia = sum(TJLF.run_tjlf(inputTJLF); dims=1)[1, :, :]
 
         for i in 1:3*inputTJLF.NS
-            @test isapprox(sum(fluxesJulia[i]),sum(fluxesFortran[i]), atol=1e-2)
+            @test isapprox(sum(fluxesJulia[i]), sum(fluxesFortran[i]), atol=1e-2)
         end
         for i in 3*inputTJLF.NS+1:4*inputTJLF.NS
-            @test isapprox(sum(fluxesJulia[i+inputTJLF.NS]),sum(fluxesFortran[i]), atol=1e-2)
+            @test isapprox(sum(fluxesJulia[i+inputTJLF.NS]), sum(fluxesFortran[i]), atol=1e-2)
         end
     end
 end
