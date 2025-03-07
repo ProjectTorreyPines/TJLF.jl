cfe73d900f7210ef6dbc1e1b02e1f594fdb3e37d
Orso Meneghini <orso82@gmail.com>
2025-03-06 22:01:45 -0800

    streamline parsing of input.tglf
    from https://github.com/ProjectTorreyPines/TJLF.jl/pull/23

--- src/tjlf_read_input.jl
+++ src/tjlf_read_input.jl
@@ -1,15 +1,16 @@
 """
     readInput(baseDirectory::String)
 
+parse through a input.tglf file found in baseDirectory parameter, creates a inputTJLF struct and populates the fields
+based off the values in the file. has some check to make sure the file is written properly
+
 parameter:
-    baseDirectory::String - string of the directory (include a '\\' at the end) of input.tglf (NOT input.tglf.gen)
+
+  - baseDirectory::String - string of the directory (include a '\\' at the end) of input.tglf (NOT input.tglf.gen)
 
 return:
-    inputTJLF::InputTJLF - return InputTJLF struct based off the input.tglf file
 
-description:
-    parse through a input.tglf file found in baseDirectory parameter, creates a inputTJLF struct and populates the fields
-    based off the values in the file. has some check to make sure the file is written properly
+  - inputTJLF::InputTJLF - return InputTJLF struct based off the input.tglf file
 """
 function readInput(filename::String)::InputTJLF
     # gets the input.tglf file
@@ -19,93 +20,105 @@ function readInput(filename::String)::InputTJLF
     ns = -1
     nky = -1
     kygrid_model = -1
-    for line in lines[1:length(lines)]
-        line = split(line, "\n")
-        line = split(line[1],"=")
-        if line[1] == "NS"
-            ns = parse(Int, strip(line[2]))
-        elseif line[1] == "NKY"
-            nky = parse(Int, strip(line[2]))
-        elseif line[1] == "KYGRID_MODEL"
-            kygrid_model = parse(Int, strip(line[2]))
+    for line in lines
+        if contains(line, '#') || !contains(line, '=')
+            continue
+        end # ignores comments in the input.tglf file
+        field, value = strip.(split(line, "="))
+        if field == "NS"
+            ns = parse(Int, strip(value))
+        elseif field == "NKY"
+            nky = parse(Int, strip(value))
+        elseif field == "KYGRID_MODEL"
+            kygrid_model = parse(Int, strip(value))
         end
     end
     # make sure ns is defined
-    @assert ns!=-1 "did not find NS in $filename (make sure this is an input.tglf file)"
-    @assert nky!=-1 "did not find NKY"
-    @assert kygrid_model!=-1 "did not find KYGRID_MODEL"
-    @assert kygrid_model>=0 && kygrid_model <=5 "KYGRID_MODEL must be Int between 0 and 5"
-    nky = get_ky_spectrum_size(nky,kygrid_model)
-
-
+    @assert ns != -1 "did not find NS in $filename (make sure this is an input.tglf file)"
+    @assert nky != -1 "did not find NKY"
+    @assert kygrid_model != -1 "did not find KYGRID_MODEL"
+    @assert kygrid_model >= 0 && kygrid_model <= 5 "KYGRID_MODEL must be Int between 0 and 5"
+    nky = get_ky_spectrum_size(nky, kygrid_model)
 
     # create InputTJLF struct
-    inputTJLF = InputTJLF{Float64}(ns,nky)
+    inputTJLF = InputTJLF{Float64}(ns, nky)
     # fields that aren't used or implemented
-    deletedFields = ["USE_TRANSPORT_MODEL","GEOMETRY_FLAG","B_MODEL_SA","FT_MODEL_SA","VPAR_SHEAR_MODEL","WRITE_WAVEFUNCTION_FLAG","VTS_SHEAR","VNS_SHEAR","VEXB","RMIN_SA","RMAJ_SA","Q_SA","SHAT_SA","ALPHA_SA","XWELL_SA","THETA0_SA","NN_MAX_ERROR"]
+    deletedFields = [
+        "USE_TRANSPORT_MODEL",
+        "GEOMETRY_FLAG",
+        "B_MODEL_SA",
+        "FT_MODEL_SA",
+        "VPAR_SHEAR_MODEL",
+        "WRITE_WAVEFUNCTION_FLAG",
+        "VTS_SHEAR",
+        "VNS_SHEAR",
+        "VEXB",
+        "RMIN_SA",
+        "RMAJ_SA",
+        "Q_SA",
+        "SHAT_SA",
+        "ALPHA_SA",
+        "XWELL_SA",
+        "THETA0_SA",
+        "NN_MAX_ERROR"
+    ]
 
     # go through each line of the input.tglf file
-    for line in lines[1:length(lines)]
-        if contains(line,'#') || !contains(line,'=') continue end # ignores comments in the input.tglf file
-        # gets rid of white space
-        line = replace(line, " "=>"")
-        # splits the line so that line[1] is the variable name and line[2] is the variable value
-        line = split(line, "\n")
-        line = split(line[1],"=")
+    for line in lines
+        if contains(line, '#') || !contains(line, '=')
+            continue
+        end # ignores comments in the input.tglf file
 
-        if line[1] ∈ deletedFields continue end 
+        # splits the line so that field is the variable name and value is the variable value
+        field, value = strip.(split(line, "="))
+
+        if field ∈ deletedFields
+            continue
+        end
 
         # check for the species field since they are all named [field name]_[species number]
-        check = match(r"_\d",line[1])
-        
-        if check !== nothing # is a species field
+        if match(r"_\d", field) !== nothing # is a species field
+            if replace(field, r"_\d" => "") ∈ deletedFields
+                continue
+            end
 
-            if replace(line[1], r"_\d"=>"") ∈ deletedFields continue end
-            # alternative way of getting field nad species number
-            # temp = split(line[1],"_")
             # get field and species number
-            speciesField = Symbol(replace(line[1], r"_\d"=>""))
-            speciesIndex = check.match[2:end]
+            speciesField, speciesIndex = rsplit(field, "_"; limit=2)
+            speciesField = Symbol(speciesField)
+            speciesIndex = parse(Int, speciesIndex)
             # skip values beyond the species number
-            if parse(Int,speciesIndex) > ns continue end
+            if speciesIndex > ns
+                continue
+            end
             # set the value to the species field vectors
-            getfield(inputTJLF, speciesField)[parse(Int,speciesIndex)] = parse(Float64,strip(line[2], ['\'','.',' ']))
+            getfield(inputTJLF, speciesField)[speciesIndex] = parse(Float64, value)
 
-        # species vector as a vector
-        elseif line[2][1] == '['
-            field = Symbol(line[1])
-            getfield(inputTJLF, field) .= [parse(Float64,item) for item in split(line[2][2:end-1],",")]
-        
+            # species vector as a vector
+        elseif startswith(value, '[')
+            getfield(inputTJLF, Symbol(field)) .= [parse(Float64, strip(item)) for item in split(value[2:end-1], ",")]
+
         else # if not for the species vector
 
-            # get the field name
-            field = Symbol(line[1])
-
             # string
-            if line[2][1] == '\'' || line[2][1] == '\"'
-                val = string(strip(line[2], ['\'']))
+            if startswith(value, '\'') || startswith(value, '\"')
+                val = string(strip(value, ['\'', '"']))
 
-            # bool
-            elseif line[2][1] == '.'
-                # make sure the input file is written correctly
-                if lowercase(strip(line[2], ['\'','.'])) != "true" && lowercase(strip(line[2], ['\'','.'])) != "false"
-                    println(line[1])
-                    error("please put quotes around your string")
-                end
-                val = lowercase(strip(line[2], ['\'','.'])) == "true"
+                # bool
+            elseif value == ".true." || value == ".false."
+                val = (lowercase(value) == ".true.")
 
-            # int
-            elseif !contains(line[2],'.')
-                val = parse(Int, strip(line[2], ['\'','.',' ']))
+                # int
+            elseif !contains(value, '.')
+                val = parse(Int, value)
 
-            # float
+                # float
             else
-                val = parse(Float64,strip(line[2], ['\'','.',' ']))
+                val = parse(Float64, value)
             end
 
             # set the inputTJLF field value
             try
-                setfield!(inputTJLF,field,val)
+                setfield!(inputTJLF, Symbol(field), val)
             catch
                 println(field)
                 println(val)
@@ -127,12 +140,11 @@ function readInput(filename::String)::InputTJLF
     inputTJLF.KY_SPECTRUM .= NaN
     inputTJLF.EIGEN_SPECTRUM .= NaN
 
-    if ismissing(inputTJLF.FIND_EIGEN)
-       inputTJLF.FIND_EIGEN = true
-    end
-
-
     # double check struct is properly populated
+
+    if ismissing(inputTJLF.FIND_EIGEN)
+        inputTJLF.FIND_EIGEN = true
+    end
 
     #If you want to test a long-format example (see tjlf_modules.jl), inP = true, otherwise, inP = false.
     #This is overly simplistic but I didn't want to mess anything else up.
@@ -140,33 +152,31 @@ function readInput(filename::String)::InputTJLF
     if inP
         inputTJLF = InputTJLF{Float64}(inP)
     end
-    
+
     #Maybe checkInput could be altered for inputting default values, or the struct in modules could be
     #Redefined as having 
     checkInput(inputTJLF)
 
     return inputTJLF
-
 end
 
 
 """
     checkInput(inputTJLF::InputTJLF)
 
-description:
-    check that the InputTJLF struct is properly populated
+check that the InputTJLF struct is properly populated
 """
 function checkInput(inputTJLF::InputTJLF)
     field_names = fieldnames(InputTJLF)
     for field_name in field_names
         field_value = getfield(inputTJLF, field_name)
-        if typeof(field_value)<:Missing
+        if typeof(field_value) <: Missing
             @assert !ismissing(field_value) "Did not properly populate inputTJLF for $field_name = $field_value"
         end
-        if typeof(field_value)<:Real
+        if typeof(field_value) <: Real
             @assert !isnan(field_value) "Did not properly populate inputTJLF for $field_name = $field_value"
         end
-        if typeof(field_value)<:Vector && field_name!=:KY_SPECTRUM && field_name!=:EIGEN_SPECTRUM
+        if typeof(field_value) <: Vector && field_name != :KY_SPECTRUM && field_name != :EIGEN_SPECTRUM
             for val in field_value
                 @assert !isnan(val) "Did not properly populate inputTJLF for array $field_name = $field_value"
             end
